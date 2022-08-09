ff25afd48448f4fed33acc10ec6f116a37850710
Tasha Pais <32210691+tashapais@users.noreply.github.com>
2022-08-09 15:10:10 -0400

    Delete Documents/Github directory

--- Documents/Github/Solana/frontend/src/createKeyPair.js
+++
@@ -1,9 +0,0 @@
-// Shoutout to Dabbit for helping w/ this!
-// https://twitter.com/dabit3
-
-const fs = require('fs')
-const anchor = require("@project-serum/anchor")
-
-const account = anchor.web3.Keypair.generate()
-
-fs.writeFileSync('./keypair.json', JSON.stringify(account))
